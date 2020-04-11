52a681b019b7ef2958b88ef56f2fbc2f7f71e745
Scott Watson <scott@haleyswill.com>
2020-04-11 09:40:19 -0400

    both hello.go and hello_test.go are in a basic state where a simple test is run and the test passes

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Hello function takes a name
+func Hello(name string) string {
+	return "Hello, " + name
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello("world"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-func testHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+func TestHello(t *testing.T) {
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
