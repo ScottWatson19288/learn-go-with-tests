4b943e90d606a3f556686ebe2d72e3b640573901
Scott Watson <scott@haleyswill.com>
2020-04-11 09:50:26 -0400

    refactored code and test, added test case for empty name

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello function takes a name
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
