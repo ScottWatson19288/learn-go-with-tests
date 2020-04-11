c18a820def9cfb78a30974ffbb865df5df61b445
Scott Watson <scott@haleyswill.com>
2020-04-11 13:47:46 -0400

    refactored & tested

--- arrayslice/sum.go
+++ arrayslice/sum.go
@@ -9,13 +9,18 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-// SumAll takes in a variable number of ints and adds them together
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
+// SumAllTails takes in a variable number of ints and adds them together
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return sums
 }
