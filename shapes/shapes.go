a4b2670dc0c72d078f09e76f1885a55f2f57a86c
Scott Watson <scott@haleyswill.com>
2020-04-12 12:57:06 -0400

    refactored, tested and working. Added table diven tests and made sure test output is hepful

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -13,6 +13,12 @@ type Circle struct {
 	Radius float64
 }
 
+// Triangle struct holds the Base and Height
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 // Shape interface that has the Area method
 type Shape interface {
 	Area() float64
@@ -31,4 +37,9 @@ func (r Rectangle) Area() float64 {
 // Area calculates the ares for a circle
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+// Area calculates the area for a triangle
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
